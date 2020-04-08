31bcdcf94645c3fc4c17d5b2e5b2f314a67abd31
yuansusu <yuansuhong195@163.com>
2020-04-08 11:25:57 +0800

    modify

--- nodejs/script.js
+++ nodejs/script.js
@@ -1,14 +1,14 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : '',
-  user     : 'wenba',
-  password : '2njkRQ3sUqW2Q8w2qoHvgVURQyjsK5hK',
-  database : 'fudao_crm_clue_02'
+  user     : 'yuansusu',
+  password : '111111111111111111111',
+  database : 'mydatabase'
 });
 
 connection.connect();
 
-connection.query('select * from fudao_crm_clue where id=3716572', function (error, results, fields) {
+connection.query('select * from mytable where id=1, function (error, results, fields) {
   if (error) throw error;
   console.log('The solution is: ', results);
 });
